dc61c5fca50b6c8a6aea6e426ad79586c0d11636
Sameer Pantvaidya <spantvaidya@deloitte.com>
2025-04-14 13:21:48 +0530

    Basics of DataBinding

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public decimal Price { get; set; }
+
+        public IEnumerable<Product_Properties> prodProperties { get; set; }
+    }
+}
